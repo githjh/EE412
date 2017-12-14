60ae429207dd8074a6956eaf2fdb705c792c9f9d
hyunho <chaos5958@gmail.com>
2017-12-14 21:41:33 +0900

    Minor Update

--- project2/dataset.py
+++ project2/dataset.py
@@ -4,25 +4,29 @@ import numpy as np
 
 RATE_TRAIN = "ee412.train"
 
-#f = open(RATE_TRAIN, 'w')
-with open(RATE_TRAIN) as f:
-	data_read = csv.reader(f, delimiter="\t")
-	data_list = list(data_read)
-	#data_list = [int(i) for i in data_read]
-	#print data_list
-#	print (data_list)
+def load_dataset(is_train=True):
+    #f = open(RATE_TRAIN, 'w')
+    with open(RATE_TRAIN) as f:
+            data_read = csv.reader(f, delimiter="\t")
+            data_list = list(data_read)
+            #data_list = [int(i) for i in data_read]
+            #print data_list
+    #	print (data_list)
 
-data_features = np.array(data_list)[:,[0,1,3]]
-data_features = data_features.astype(int)
-data_labels = np.array(data_list)[:,[2]]
-data_labels = data_labels.astype(int)
+    data_features = np.array(data_list)[:,[0,1,3]]
+    data_features = data_features.astype(np.int32)
+    data_labels = np.array(data_list)[:,[2]]
+    data_labels = data_labels.astype(np.int32)
 
+    return data_features, data_labels
 
-print type(data_labels[0])
-assert data_features.shape[0] == data_labels.shape[0]
+#print(type(data_labels[0]))
+#assert data_features.shape[0] == data_labels.shape[0]
 
+"""
 dataset = tf.data.Dataset.from_tensor_slices((data_features, data_labels))
 batched_dataset = dataset.batch(4)
 iterator = batched_dataset.make_one_shot_iterator()
 next_example, next_label = iterator.get_next()
-print (next_example, next_label)
+print((next_example, next_label))
+"""
