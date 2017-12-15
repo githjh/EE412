b4cbd20d335e41b6dc96dfeef7fdc5644d7e3279
githjh <thundershower@kaist.ac.kr>
2017-12-15 22:18:05 +0900

    test set updata

--- project2/dataset.py
+++ project2/dataset.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 
 RATE_TRAIN = "ee412.train"
-
+RATE_TEST = "ee412.testset.csv"
 TEST_DATA_SIZE = 10000
 TIME_MIN = 874700000
 TIME_MAX = 893300000
@@ -13,22 +13,31 @@ ITEM_MAX = 1682
 DATA_FEATURE_SIZE = USER_MAX + ITEM_MAX + 1
 
 def load_dataset(is_train=True):
-    
-    with open(RATE_TRAIN) as f:
+    File_name = None
+    if(is_train):
+	File_name = RATE_TRAIN
+    else:
+	File_name = RATE_TEST
+    with open(File_name) as f:
+    	if(is_train):
             data_read = csv.reader(f, delimiter="\t")
-            data_list = list(data_read)
-	    data_len = len(data_list)
-	    train_data_end = data_len - TEST_DATA_SIZE
+	else:
+	    data_read = csv.reader(f, delimiter=",")
+	data_list = list(data_read)
+	    #data_len = len(data_list)
+	    #train_data_end = data_len - TEST_DATA_SIZE
 	    #find_data = np.array(data_list)[:,[3]]
 	    #find_data = find_data.astype(np.float32)
 	    #print np.amax(find_data),np.amin(find_data)
-	    if (is_train):
-		    data_list = data_list[:train_data_end]
-	    else:
-		    data_list = data_list[train_data_end:]
-    #	print (data_list)
-
-    data_features_raw = np.array(data_list)[:,[0,1,3]]
+	    #if (is_train):
+	    #	data_list = data_list[:train_data_end]
+	    #else:
+	    #	data_list = data_list[train_data_end:]
+    #print data_list
+    if(is_train):
+    	data_features_raw = np.array(data_list)[:,[0,1,3]]
+    else:
+    	data_features_raw = np.array(data_list)[:,[0,1,2]]
     data_features_raw = data_features_raw.astype(np.float32)
     data_features_raw[:,[2]] = (data_features_raw[:,[2]] - TIME_MIN )/(TIME_MAX-TIME_MIN)
 
@@ -45,11 +54,14 @@ def load_dataset(is_train=True):
     #np.set_printoptions(threshold=np.nan)
 
     #print (data_features[0:2])
-    data_labels = np.array(data_list)[:,[2]]
-    data_labels = data_labels.astype(np.float32)
+    if(is_train):
+    	data_labels = np.array(data_list)[:,[2]]
+	data_labels = data_labels.astype(np.float32)
+    else:
+	data_labels = np.zeros((dataset_size,1))
 
     return data_features, data_labels
 
-#features, labels = load_dataset()
+#features, labels = load_dataset(False)
 #print (features.shape,labels.shape)
 
